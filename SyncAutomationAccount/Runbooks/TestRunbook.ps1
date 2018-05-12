d52bc47c9980434ba9c0fdd055d2e4fafd1f9df3
Marco Brouwer <marco.brouwer@nn.nl>
2018-05-12 13:06:55 +0200

    Moved AuthKey to RequestHeader

--- SyncAutomationAccount/Runbooks/TestRunbook.ps1
+++ SyncAutomationAccount/Runbooks/TestRunbook.ps1
@@ -3,23 +3,23 @@ param([object]$WebHookData)
 if($WebHookData)
 {
     $requestBody = ($WebHookData.RequestBody | ConvertFrom-JSON)
+    $requestHeader = $WebHookData.RequestHeader
 }
 
-if ($requestBody.message -eq '##AuthKey##')
+if ($requestHeader.AuthKey -eq '##AuthKey##')
 {
     Write-Output "Webhook authenticated"
 } else {
-    Write-Output "Got unauthenticated request :( ";
+    Write-Output "Got unauthenticated request"
     exit
 }
 
-"Logging in to Azure..."
+Write-Output "Logging in to Azure..."
 $connectionName = "AzureRunAsConnection"
 $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName         
-Add-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+# Add-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
 
 # $automationCredential = Get-AutomationPSCredential -Name 'MyAccount'
-
 # Add-AzureRmAccount -Credential $automationCredential
 
-Get-AzureRMResource
+# Get-AzureRMResource
